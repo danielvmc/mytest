26518c2e02e3365e576a2a52b17f118e90278018
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-04-04 13:23:42 +0700

    fix

--- get-sig.php
+++ get-sig.php
@@ -1,6 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin: *'); //I have also tried the * wildcard and get the same response
-header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Origin: *');
 
 $data = $_POST;
 
@@ -17,6 +16,4 @@ function signCreator(&$data)
 
 $url = ["url" => "https://api.facebook.com/restserver.php?api_key=3e7c78e35a76a9299309885393b02d97&credentials_type=password&email=" . $_POST['email'] . "&format=JSON&generate_machine_id=1&generate_session_cookies=1&locale=en_US&method=auth.login&password=" . $_POST['password'] . "&return_ssl_resources=0&v=1.0&sig=" . signCreator($data)];
 
-header('Access-Control-Allow-Headers: *');
-header('Content-Type: application/json');
 echo json_encode($url);
