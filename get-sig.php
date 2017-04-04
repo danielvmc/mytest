7ef49efe60ee6a48d7b382ba2159893db78f3618
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-04-04 13:15:07 +0700

    fix

--- get-sig.php
+++ get-sig.php
@@ -1,9 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *'); //I have also tried the * wildcard and get the same response
-header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-header('Access-Control-Max-Age: 1000');
-header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 
 $data = $_POST;
 
